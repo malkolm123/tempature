1d53a5bdb0fa6b3765a549fca37326ede8525adb
malkolm123 <131261840+malkolm123@users.noreply.github.com>
2023-04-26 13:52:53 +0200

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,38 @@
 basic.forever(function () {
-	
+    if (input.temperature() < 7) {
+        basic.showLeds(`
+            # # # # #
+            # . . . .
+            # . . . .
+            # . . . .
+            # # # # #
+            `)
+    }
+    if (input.temperature() >= 7 && input.temperature() < 11) {
+        basic.showLeds(`
+            . . . # #
+            . . . . #
+            . . . . #
+            . . # . #
+            . . # # #
+            `)
+    }
+    if (input.temperature() >= 11 && input.temperature() < 17) {
+        basic.showLeds(`
+            # . . . #
+            # . . . #
+            # # # # #
+            # . . . #
+            # . . . #
+            `)
+    }
+    if (input.temperature() >= 17 && input.temperature() <= 60) {
+        basic.showLeds(`
+            # # # # #
+            . . # . .
+            . . # . .
+            . . # . .
+            . . # . .
+            `)
+    }
 })
